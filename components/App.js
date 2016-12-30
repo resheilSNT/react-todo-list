90e9c515325805c62405b2ae10fa816466907d33
resheil mohammed <resheilmohammed@resheils-MBP.fios-router.home>
2016-12-30 12:25:21 -0500

    initial changes

--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React , {Component} from 'React'
 
+
+class App extends Component {
+	render(){
+		return <div>This is React App now</div>
+	}
+}
+
+export default App
+// exporting is required for importing
